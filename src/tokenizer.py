9f588da177c1ac3e6a2e7b988c2e5783ab2775a0
primrose <yakup.paradox@gmail.com>
2022-01-13 21:51:47 +0100

    improve tokenizer, add negation, iterate over all files and write tokenizations

--- src/tokenizer.py
+++ src/tokenizer.py
@@ -1,9 +1,10 @@
-import io
+import os
 import sys
 import csv
 import nltk
 import re
 import vocab
+import constants
 nltk.download('punkt')
 
 
@@ -13,7 +14,7 @@ def arr_to_string(arr):
     if len(arr) > 1:
         for i in range(len(arr)-1):
             s += arr[i]
-            s += ", "
+            s += "; "
         s += arr[len(arr)-1]
         #s += "]"
     elif len(arr) == 1:
@@ -28,10 +29,10 @@ def string_to_arr(str):
     #"[ ..., a..., "
     # read until first ,
     # skip one offeset, repeat
-    if not "," in str:
+    if not ";" in str:
         print(str)
     else:
-        x = str.split(", ")
+        x = str.split("; ")
         print(x)
 
 
@@ -59,10 +60,10 @@ def abbreviation_mapper(i_map, abbrv_len, vec):
 
         outp = str()
         if check_str in i_map.keys():
-            e = "found a: "
-            e += check_str
-            e += "\n"
-            sys.stderr.write(e)
+            # e = "found a: "
+            # e += check_str
+            # e += "\n"
+            # sys.stderr.write(e)
             outp = i_map[check_str]
         else:
             outp = vec[i]
@@ -79,34 +80,72 @@ def abbreviation_mapper(i_map, abbrv_len, vec):
 def alias_mapper(vec):
     t = abbreviation_mapper(vocab.aliases_11, 1, vec)
     t = abbreviation_mapper(vocab.aliases_21, 2, t)
-    t = abbreviation_mapper(vocab.aliases_31, 3, vec)
+    t = abbreviation_mapper(vocab.aliases_31, 3, t)
     t = abbreviation_mapper(vocab.aliases_41, 4, t)
     return t
 
 
-def tokenize(filepath, body_offset):
-    file = open(filepath)
-    csv_reader = csv.reader(file)
-
-    first = True
-    for row in csv_reader:
-        if first:
-            first = False
-            continue
-        else:
-            raw_text = row[body_offset]
-            # print(raw_text)
-            letters_only = re.sub("[^a-zA-Z]", " ", raw_text)
-            # print(letters_only)
-            raw_tokens = nltk.word_tokenize(letters_only)
-            # print(raw_tokens)
-            for i in range(len(raw_tokens)):
-                raw_tokens[i] = raw_tokens[i].lower()
-            # print(raw_tokens)
-            # print(arr_to_string(raw_tokens))
-            # string_to_arr(arr_to_string(raw_tokens))
-            tt = alias_mapper(raw_tokens)
-            print(tt)
+def negate(vec):
+    for i in range(len(vec)-1):
+        if vec[i] == "no" or vec[i] == "not":
+            vec[i] = "n"
+            vec[i+1] = "-" + vec[i+1]
 
 
-tokenize("fds_data/fds_comments-2019-12.csv", 2)
+def clear_len_1(vec):
+    res = [i for i in vec if len(i) > 1]
+    return res
+
+
+def clear_len_3(vec):
+    res = [i for i in vec if len(i) > 3]
+    return res
+
+
+def tokenize(filepath, body_offset, outputfile):
+    if os.path.exists(filepath):
+        file = open(filepath)
+
+        csv_reader = csv.reader(file)
+
+        if os.path.exists(outputfile):
+            os.remove(outputfile)
+        outf = open(outputfile, "w")
+        writer = csv.writer(outf)
+
+        first = True
+        for row in csv_reader:
+            if first:
+                first = False
+                writer.writerow(row)
+                continue
+            else:
+                raw_text = row[body_offset]
+                letters_only = re.sub("[^a-zA-Z]", " ", raw_text)
+                raw_tokens = nltk.word_tokenize(letters_only)
+
+                for i in range(len(raw_tokens)):
+                    raw_tokens[i] = raw_tokens[i].lower()
+
+                tt = alias_mapper(raw_tokens)
+                tt = clear_len_1(tt)
+                negate(tt)
+                tt = clear_len_3(tt)
+
+                row[body_offset] = arr_to_string(tt)
+                writer.writerow(row)
+
+
+subs = ["fds", "bps"]
+
+for sub in subs:
+    for (c, offset) in [("comments", 2), ("posts", 3)]:
+        for y in constants.years_asc:
+            for m in constants.months:
+                inf = sub + "_data/" + sub + "_" + c + \
+                    "-" + str(y) + "-" + str(m) + ".csv"
+
+                outf = sub + "_data/" + sub + "_" + c + "-" + \
+                    str(y) + "-" + str(m) + "-tokenized.csv"
+
+                tokenize(inf, offset, outf)
